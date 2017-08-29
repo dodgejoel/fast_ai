50338982fb1b546d1ef8ea94ab4832ff4595e687
joel <dodgejoel@infer.com>
2017-08-29 15:50:57 -0700

    some more setup utils

--- project_setup.py
+++ project_setup.py
@@ -4,8 +4,51 @@ import shutil
 
 from os import path
 from Queue import Queue
+import glob
 
-random.seed(57)  # my favorite prime
+rand = random.Random()
+rand.seed(57)  # my favorite prime
+
+def make_folds(proot, num_folds):
+  """
+  if your data are able to be split into folds directly without any
+  entanglement between items then this will do the trick
+  """
+  for class_ in os.listdir(path.join(proot, 'train')):
+    for i in range(num_folds):
+      os.makedirs(path.join(proot, 'fold_%d' % i, class_))
+    imgs = os.listdir(path.join(proot, 'train', class_))
+    for i in imgs:
+      src = path.join(proot, 'train', class_, img)
+      dest = path.join(proot, 'fold_%d' % rand.randint(0, num_folds - 1), class_, img)
+      print('moving from %s to %s' % (src, dest))
+      shutil.move(src, dest)
+
+def make_folds_sample(proot, num):
+  """
+  make a sample directory reproducing the structure of the fold_* and test dirs
+  in proot but wih only num files per directory
+  """
+  folds = [f for f in os.listdir(proot) if f.startswith('fold_')]
+  for f in folds:
+    classes = os.listdir(path.join(proot, f))
+    for clss in classes:
+      os.makedirs(path.join(proot, 'sample', f, clss))
+      imgs = os.listdir(path.join(proot, f, clss))
+      rand.shuffle(imgs)
+      for i in imgs[:num]:
+        src = path.join(proot, f, clss, i)
+        dest = path.join(proot, 'sample', f, clss)
+        print('moving from %s to %s' % (src, dest))
+        shutil.copy(src, dest)
+
+  os.makedirs(path.join(proot, 'sample', 'test', 'unknown'))
+  for f in rand.sample(os.listdir(path.join(proot, 'test', 'unknown')), num):
+    src = path.join(proot, 'test', 'unknown', f)
+    dest = path.join(proot, 'sample', 'test', 'unknown', f)
+    print('copying %s to %s' % (src, dest))
+    shutil.copy(src, dest)
+
 
 def make_validation_set(proot, proportion=.1):
   """
@@ -18,7 +61,7 @@ def make_validation_set(proot, proportion=.1):
     os.makedirs(path.join(proot, 'valid', class_))
     imgs = os.listdir(path.join(proot, 'train', class_))
     take = int(proportion * len(imgs))
-    random.shuffle(imgs)
+    rand.shuffle(imgs)
     for img in imgs[:take]:
       src = path.join(proot, 'train', class_, img)
       dest = path.join(proot, 'valid', class_, img)
@@ -42,7 +85,7 @@ def make_sample(proot, num):
   for d in to_cp:
     for class_ in os.listdir(path.join(proot, d)):
       os.makedirs(path.join(proot, 'sample', d, class_))
-      chosen = random.sample(os.listdir(path.join(proot, d, class_)), num)
+      chosen = rand.sample(os.listdir(path.join(proot, d, class_)), num)
       for f in chosen:
         src = path.join(proot, d, class_, f)
         dest = path.join(proot, 'sample', d, class_, f)
@@ -50,7 +93,7 @@ def make_sample(proot, num):
         shutil.copy(src, dest)
               
   os.makedirs(path.join(proot, 'sample', 'test'))
-  for f in random.sample(os.listdir(path.join(proot, 'test')), num):
+  for f in rand.sample(os.listdir(path.join(proot, 'test')), num):
     src = path.join(proot, 'test', f)
     dest = path.join(proot, 'sample', 'test', f)
     print('copying %s to %s' %(src, dest))
---
+++ statefarm_util.py
@@ -0,0 +1,47 @@
+from os import path
+import csv
+import os
+import random
+import shutil
+
+rand = random.Random()
+rand.seed(57)
+
+
+def make_driver_folds(proot, num_folds):
+  drivers_file = open(path.join(proot, 'driver_imgs_list.csv'))
+  rows = list(csv.DictReader(drivers_file))
+  drivers = list(set(row['subject'] for row in rows))
+  rand.shuffle(drivers)
+  skip = len(drivers) / num_folds
+  driver_folds = [drivers[i* skip:(i + 1) * skip] for i in range(num_folds)]
+  leftovers = drivers[(i + 1) * skip:]
+  for fold, one_more in zip(driver_folds, leftovers):
+    fold.append(one_more)
+
+  fold_map = dict()
+  for i, fold in enumerate(driver_folds):
+    for driver in fold:
+      fold_map[driver] = i
+
+  for row in rows:
+    clss = row['classname']
+    driver = row['subject']
+    srcdir = path.join(proot, 'train', clss)
+    destdir = path.join(proot, 'fold_%d' % fold_map[driver], clss)
+    src = path.join(srcdir, row['img'])
+    dest = path.join(destdir, row['img'])
+    try:
+      shutil.move(src, dest)
+    except IOError:
+      # lazily create dirs
+      os.makedirs(destdir)
+      shutil.move(src,dest)
+
+
+def make_test_classy(proot):
+  # os.makedirs(path.join(proot, 'test', 'unknown'))
+  for img in os.listdir(path.join(proot, 'test')):
+    if img == 'unknown':
+      continue
+    shutil.move(path.join(proot, 'test', img), path.join(proot, 'test', 'unknown', img))
